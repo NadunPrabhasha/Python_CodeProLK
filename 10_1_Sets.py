3636a2a4c652f3c5b3421e023e4346bab6cf2e2a
Nadun <131152766+NadunPrabhasha@users.noreply.github.com>
2025-12-23 08:24:07 +0530

    update sets file

---
+++ 10_1_Sets.py
@@ -0,0 +1,18 @@
+#Sets are unorderd data types
+#cant store duplicate values only unic values only
+  
+
+my_list = ['kamal', 20, 'jaffna', False, 20]
+my_tuple = ('kamal', 20, 'jaffna', False, 20)
+
+my_set = {'kamal', 20, 'jaffna', False, 20}
+
+
+print(type(my_set))
+print(my_set)
+
+A = {1, 2, 3, 5, 7}
+B = {5, 7, 9, 10, 11}
+
+print(A.intersection(B))
+print(A.)
