6dd4b5d46a66dbe7c8333e569ffadf9a17bbc182
Nadun <131152766+NadunPrabhasha@users.noreply.github.com>
2025-12-30 08:59:40 +0530

    update code

---
+++ create_method/cal.py
@@ -0,0 +1,20 @@
+# addition
+def add(a,d):
+    return a+b 
+
+# substraction
+def sub(a,b):
+    return a-b
+
+
+# multiplication
+def mul(a,b):
+    return a*b
+
+
+# division
+
+def div(a,b):
+    return a/b
+
+
